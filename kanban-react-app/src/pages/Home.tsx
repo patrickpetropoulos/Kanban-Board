296359d13f80c1213f06a4516c46340d622cbcb7
patrickpetropoulos <33436629+patrickpetropoulos@users.noreply.github.com>
2024-02-17 22:43:52 -0500

    Initial commit on develop, testing github action

--- kanban-react-app/src/pages/Home.tsx
+++ kanban-react-app/src/pages/Home.tsx
@@ -4,6 +4,7 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to the Home Page!</h1>
+      <p>This is from the develop branch!</p>
       <p>This is the home page of our React app. Feel free to explore!</p>
     </div>
   );
